2dab4fc057fbfd113c482a484ab55eb9f059636d
Skyfive32 <88442429+Skyfive32@users.noreply.github.com>
2021-10-27 11:58:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
-
+@st.cache
+def get_data():
+    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
+    return pd.read_csv(url)
+df = get_data()
